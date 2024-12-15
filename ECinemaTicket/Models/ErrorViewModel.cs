e05365a4003c235ef42d5bb332daa6284571d5b9
Administrator <Administrator@DESKTOP-FC84KVD>
2024-12-15 13:49:21 +0500

    Add project files.

---
+++ ECinemaTicket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECinemaTicket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
